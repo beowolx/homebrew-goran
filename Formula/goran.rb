5a78d4c40b76930cccc30f1623b901e4bb9b9607
Luis Cardoso <61982523+beowolx@users.noreply.github.com>
2025-06-28 08:15:56 +0200

    Update goran to v0.1.8

--- Formula/goran.rb
+++ Formula/goran.rb
@@ -1,8 +1,8 @@
 class Goran < Formula
   desc "Domain & IP Analysis CLI"
   homepage "https://github.com/beowolx/goran"
-  url      "https://github.com/beowolx/goran/releases/download/v0.1.7/goran-0.1.7-x86_64-apple-darwin.tar.gz"
-  sha256   "21fe94fda45912fb331f88c9990b2378dae949d21a1017944d5833fc269d04f9"
+  url      "https://github.com/beowolx/goran/releases/download/v0.1.8/goran-0.1.8-x86_64-apple-darwin.tar.gz"
+  sha256   "6bd20046c277c82d4f659a6346de62a79f2873d28b6c138504910935879404e3"
   license  "MIT"
 
   def install
